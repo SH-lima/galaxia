439cc692a14cdaf83bfe4ddad9b5ea5540d468fd
SH-LIMA <haliamsaid.pro@gmail.com>
2021-02-02 11:03:15 +0100

    third commit

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 const shuttle = document.querySelector("#shuttle");
 const laser = document.querySelector("#laser");
 let i = 5
+
+const initial=()=>{
+        laser.style.display=""    
+        laser.classList.add("animation")
+        laser.classList.add("top") 
+        
+        
+        console.log("Hello")
+    }
+ 
+
 window.addEventListener("keydown", function(e){
     console.log(e.keyCode)
     let numero=e.keyCode;
     let unite = 10
 
+    
     if(numero==32){
-    laser.classList.remove("show")    
-    laser.classList.add("animation")
+        initial()
+        
     }
     
     if(numero==37){
         if(i<=0){
             unite=0
-        }
+        }else{
         i--;
         shuttle.style.left=`${unite*i}%`;
         laser.style.left=`${unite*i+1.5}%`; 
+        }
+
     }else if(numero==39){
         if(i>=9){
             i=9
